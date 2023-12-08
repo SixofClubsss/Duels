493fb49c5c6898a050d7625d1384420b61530126
Six <84689659+SixofClubsss@users.noreply.github.com>
2023-12-08 20:35:18 +0000

    store funcs used from gnomes

--- duel/duel.go
+++ duel/duel.go
@@ -2,6 +2,8 @@ package duel
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"strconv"
 	"strings"
 	"sync"
@@ -1198,3 +1200,40 @@ func claimClaimable(claimable []string, d *dreams.AppObject) {
 // 	obj.Objects[0].(*container.Split).Trailing.(*fyne.Container).Objects[1] = reset
 // 	obj.Objects[0].(*container.Split).Trailing.Refresh()
 // }
+
+// Download NFA SCID icon image file as []byte
+func downloadBytes(scid string) ([]byte, error) {
+	client := http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Get(findCollectionURL(scid))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	image, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return image, nil
+}
+
+// Find which images should be used for a collection, icon or main file
+func findCollectionURL(scid string) (url string) {
+	if gnomon.IsReady() {
+		w := 1
+		coll, _ := gnomon.GetSCIDValuesByKey(scid, "collection")
+		if coll != nil {
+			switch coll[0] {
+			case "High Strangeness":
+				w = 0
+			default:
+				// nothing, w 1 is icon
+			}
+
+			url = gnomes.GetAssetUrl(w, scid)
+		}
+	}
+
+	return
+}
